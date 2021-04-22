f372557567d4d37ea201b9ecf5996daec15e2887
qhthu <thuxyz7777@gmail.com>
2021-04-22 09:41:57 +0700

    thu

---
+++ DemoImmutableObject.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+/**
+ *
+ * @author quach
+ */
+public class Demo {
+    private final int a;
+    private final int b;
+
+    public Demo(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public static void main(String[] args) {
+
+        Demo d = new Demo(10, 20);
+        System.out.println(d.a);
+       
+    }
+
+}
