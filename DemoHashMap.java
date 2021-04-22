737bc9d616c787b7d90b20ce45e4543c9d3f0fbb
qhthu <thuxyz7777@gmail.com>
2021-04-22 11:03:18 +0700

    thu

--- DemoHashMap.java
+++ DemoHashMap.java
@@ -9,30 +9,66 @@ package demo;
  *
  * @author quach
  */
+class Name {
+
+    private String ho;
+    private String ten;
+
+    public Name(String ho, String ten) {
+        this.ho = ho;
+        this.ten = ten;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+}
+
 public class DemoHashMap {
 
     public static void main(String[] args) {
         HashMap<String, String> hash = new HashMap<>();
 
-        hash.put("quach", "hong thu");
-        hash.put("vo thi", "hong nhung");
-        hash.put("ha thi", "mai anh");
-        hash.put("nguyen", "thanh truc");
+        HashMap<Integer, Name> hashObj = new HashMap<>();
+        Name name = new Name("", "");
+        name.setHo("Quach");
+        name.setTen("Hong Thu");
+        hashObj.put(1, name);
 
-        Iterator<Map.Entry<String, String>> iterator = hash.entrySet().iterator();
+        Name name1 = new Name("", "");
+        name1.setHo("Ha Thi");
+        name1.setTen("Mai Anh");
+        hashObj.put(2, name1);
+        
+                Name name2 = new Name("", "");
+        name2.setHo("Vo Thi");
+        name2.setTen("Hong Nhung");
+        hashObj.put(3, name2);
+        
+        
+        hashObj.remove(3);
+        
+        
+        hashObj.replace(1, name1);
+        
+        Iterator<Map.Entry<Integer, Name>> iterator = hashObj.entrySet().iterator();
 
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-        System.out.println("");
-
-        hash.remove("nguyen");
-
-        hash.replace("ha thi", "minh thuan");
-
-        Iterator<Map.Entry<String, String>> iterator1 = hash.entrySet().iterator();
-        while (iterator1.hasNext()) {
-            System.out.println(iterator1.next());
+            Name n = (Name) iterator.next().getValue();
+            System.out.println(n.getHo() + " " + n.getTen());
         }
 
     }
